1e44ee8be3a34e542a1689f60764368ef6e8764c
snail007 <arraykeys@gmail.com>
2023-04-29 19:55:42 +0800

    Update install_auto.sh

--- install_auto.sh
+++ install_auto.sh
@@ -16,7 +16,10 @@ echo -e ">>> installing ... \n"
 #install proxy-admin
 tar zxvf $F >/dev/null 2>&1 
 chmod +x proxy-admin
+mkdir -p /usr/local/bin/
+cp -f proxy-admin /usr/local/bin/
 set +e
+cd /usr/local/bin/
 ./proxy-admin uninstall >/dev/null 2>&1 
 set -e
 ./proxy-admin install
@@ -27,8 +30,8 @@ systemctl status proxyadmin &
 set -e
 sleep 2
 echo  -e "\n>>> install done, thanks for using snail007/proxy-admin\n"
-echo  -e ">>> install path /usr/bin/proxy\n"
-echo  -e ">>> configuration path /etc/proxy\n"
-echo  -e ">>> uninstall just exec : rm /usr/bin/proxy && rm /etc/proxy\n"
+echo  -e ">>> install path /usr/local/bin/proxy-admin\n"
+echo  -e ">>> configuration path /etc/gpa\n"
+echo  -e ">>> uninstall just exec : /usr/local/bin/proxy-admin uninstall && rm /etc/gpa\n"
 echo  -e ">>> please visit : http://YOUR_IP:32080/ username: root, password: 123\n"
 echo  -e ">>> How to using? Please visit : $manual\n"
